817ffdc52ad3dd6793ae858e4dae6af6147743c8
Ben Douglas <bensfilter11@gmail.com>
2023-12-13 21:13:06 -0600

    cleaned up code, passes the test  but it still doesn't work

--- AdventOfCode2023/day12.py
+++ AdventOfCode2023/day12.py
@@ -1,45 +1,58 @@
 test = True
 
 def drillDown(symbolsIn, symbolIndex ,groupsIn, groupIndex):
-    if groupIndex == len(groupsIn):
-        return 1 # all groups found, return 1
-    if groupIndex > len(groupsIn) or symbolIndex >= len(symbolsIn):
+    # padded inital symbolIn list with '.' makes processing easier
+    # determine if the symbols from index symbolIndex to groupsIn[groupIndex] are not '.'
+    # and determine if the immediate next character is not '#' or the end of the string.
+
+    if groupIndex >= len(groupsIn):
+        print('Error, past the end of the group List')
         return 0 # no matches here, return
     
     groupSize = int(groupsIn[groupIndex])
-    # if symbolIndex + groupSize >= len(symbolsIn):
-    #     return 0 # no room for possible matches
-    
-    remainingStringSize = len(symbolsIn) - symbolIndex
-    remainingGroupSize = 0
-    for group in groupsIn[groupIndex:]:
-        remainingGroupSize+=int(group)
-    
-    if remainingGroupSize > remainingStringSize:
-        return 0 # no more possible matches
 
+    if symbolIndex + groupSize > len(symbolsIn):
+        # max length is the symbolIndex + the current group size; 
+        return 0
+    
+    # print(f'symbolIndex = {symbolIndex} : groupIndex = {groupIndex}')
+    
+    if symbolsIn[symbolIndex-1] == '#':
+        # previous symbol make invalid combo, don't continue
+        print(f'Found invalid at {symbolIndex} for group {groupIndex}')
+        return 0
+    
     output = 0
-    for index in range (symbolIndex,len(symbolsIn)-remainingGroupSize+1):
-        if symbolsIn[index:index+groupSize].count('.') == 0:
+
+    for index in range (symbolIndex,len(symbolsIn)):
+
+        if symbolsIn[index:index+groupSize].count('.') == 0 and symbolsIn[index-1] != '#' :
             if groupIndex == len(groupsIn)-1:
+                # last in the group is found, add 1
                 output+=1
             else:
                 # TODO account for the space between groups
                 if symbolsIn[index+groupSize] != '#':
                     output += drillDown(symbolsIn, index+groupSize+1, groupsIn, groupIndex+1)
+                # else:
+                #     print(f'Found invalid 2 at {index}')
     
     return output
 
 
+lines = open("test.txt").readlines() if test else open('day12.txt').readlines() 
 
-
-lines = open('AdventOfCode2023/test.txt').readlines() if test else open('AdventOfCode2023/day12.txt').readlines() 
+part1 = 0
 
 for line in lines:
     [symbolsStr, groupsStr] = line.split(' ')
     groups = groupsStr.strip().split(',')
-    print(groups)
+    
+    # padded symbolsStr makes processing easier
+    temp = drillDown('.'+symbolsStr+'.',1,groups,0)
+    print(f'{groups} => {temp}')
+    part1 += temp
 
-    print(drillDown(symbolsStr,0,groups,0))
+print(part1)
 
 
