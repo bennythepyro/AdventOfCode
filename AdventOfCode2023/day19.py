9757f94f95fc50f7072f2d0585aa0972ff85b3ff
Ben Douglas <bensfilter11@gmail.com>
2023-12-29 11:19:18 -0600

    completed day 19 part 2

--- AdventOfCode2023/day19.py
+++ AdventOfCode2023/day19.py
@@ -1,4 +1,4 @@
-test= True
+test= False
 
 lines = open("C:\\Users\\Ben\\Workspace\\PublicShare\\AdventOfCode2023\\test.txt").readlines() if test \
     else open('C:\\Users\\Ben\\Workspace\\PublicShare\\AdventOfCode2023\\day19.txt').readlines() 
@@ -68,4 +68,82 @@ for part in myParts:
             part1+= val
 
 print(part1)
+part2=0
+def fillRangeThisPath(theList, theRange: int, theOperation: str):
+    theFillValue = False
+    match theOperation:
+        # make everything outside of the range given false
+        case 'LT':
+            # print(f'LT : {len(theList[theRange-1:])} : {4000-theRange + 1}')
+            theList[theRange-1:] = [theFillValue] * (4000-theRange + 1)
+        case 'GT':
+            # print(f'GT : {len(theList[:theRange])} : {theRange}')
+            theList[:theRange] = [theFillValue] * theRange
+        case '':
+            return
+
+def fillRangeMissedPath(theList, theRange: int, theOperation: str):
+    theFillValue = False
+    # make everything in the range false
+    match theOperation:
+        case 'GT':
+            # print(f'GT : {len(theList[theRange:])} : {4000-theRange}')
+            theList[theRange:] = [theFillValue] * (4000-theRange) 
+        case 'LT':
+            # print(f'LT : {len(theList[:theRange-1])} : {theRange-1}')
+            theList[:theRange-1] = [theFillValue] * (theRange-1) 
+        case '':
+            return
+
+def drillDown(key:str, instrList, skippedInstrs):
+    global part2
+    if key == 'A':
+        # apply the instrList to the ranges
+        # print("vvvvvvvvvvvvvvvvvvvvvvvv")
+        # # print(f'Key = {key}')
+        # print(*instrList,sep='\n')
+        # print()
+        # print(*skippedInstrs,sep='\n')
+        # print("^^^^^^^^^^^^^^^^^^^^^^^^")
+
+        ranges = { 'x' : [True for x in range(0,4000)],
+                    'm' : [True for x in range(0,4000)],
+                    'a' : [True for x in range(0,4000)],
+                    's' : [True for x in range(0,4000)]}
+
+        for instr in instrList:
+            if instr[0] != '':
+                fillRangeThisPath(ranges[instr[0]],instr[2],instr[1])
+
+            # match instr[0]:
+            #     case 'x':
+            #         fillRangeThisPath(xRange,instr[2],instr[1])
+            #     case 'm':
+            #         fillRangeThisPath(mRange,instr[2],instr[1])
+            #     case 'a':
+            #         fillRangeThisPath(aRange,instr[2],instr[1])
+            #     case 's':
+            #         fillRangeThisPath(sRange,instr[2],instr[1])
+        # print(f'X = {ranges["x"].count(True)} ; M = {ranges["m"].count(True)} ; A = {ranges["a"].count(True)} ; S = {ranges["s"].count(True)} ; ')
+        # TODO fill with the missed paths
+        for instr in skippedInstrs:
+            if instr[0] != '':
+                fillRangeMissedPath(ranges[instr[0]],instr[2],instr[1])
+        # print(f'X = {ranges["x"].count(True)} ; M = {ranges["m"].count(True)} ; A = {ranges["a"].count(True)} ; S = {ranges["s"].count(True)} ; ')
+        # print(f'X = {len(ranges["x"])} ; M = {len(ranges["m"])} ; A = {len(ranges["a"])} ; S = {len(ranges["s"])} ; ')
+        part2 += ranges["x"].count(True)*ranges["m"].count(True)*ranges["a"].count(True)*ranges["s"].count(True)
+        return
     
+    if key == 'R':
+        # reject is default, do nothing
+        return
+    newSkippedInstrs = skippedInstrs.copy()
+    for instr in myWorkflows[key].instructions:
+        temp = instrList.copy()
+        temp.append(instr)
+        drillDown(instr[3],temp,newSkippedInstrs)
+        newSkippedInstrs.append(instr)
+
+
+drillDown('in',[],[])
+print(part2)
